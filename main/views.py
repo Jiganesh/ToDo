871202cfb0faa470b023d71a479eb58b47a8c9d4
Jiganesh <jiganeshpatil01071999@gmail.com>
2021-05-13 00:18:53 +0530

    initial

---
+++ main/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+from django.utils import timezone #to record current-time
+from .models import Todo
+
+# Create your views here.
+
+def index(request):
+    todo_items =Todo.objects.all().order_by("added_date")
+    return render(request, 'main/list.html',{"todo_items": todo_items})
+
+
+def add_todo(request):
+
+    content_to_add= request.POST['content']  #get from form with name content
+
+
+    if content_to_add.replace(" ","") != "":
+        current_date = timezone.now() #to get time
+
+        created_obj = Todo.objects.create(added_date=current_date, text=content_to_add)
+        
+        print(created_obj.id)
+        print(Todo.objects.all().count())
+
+
+    return HttpResponseRedirect("/")
+
+def delete_todo(request,todo_id):
+    if Todo.objects.all().count() >0:
+        Todo.objects.get(id=todo_id).delete()
+    return HttpResponseRedirect("/")
